4bfc8d9338d6453c67ea04efcd26c8b6cb638180
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-02-26 14:54:09 +0200

    Installed skeleton rails App with Rspec and Capybara.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = '5f1b4fd8bda6fd6517c426374ca2a14650078cd622d4dd02f43784443af9d5c7049350975bb9c52da7fe3b6d4d727e0a535bebaeb212e35e8966648081d1d4e7'
